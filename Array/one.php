19a6a71c9ffaa4600fa75adde5f536fabeb08275
Vikash Nayan <imvikashnayan@gmail.com>
2024-08-16 12:29:51 +0530

    while

---
+++ Array/one.php
@@ -0,0 +1,24 @@
+<?php  
+// $num=12;  
+// if($num<100){  
+// echo "$num is less than 100";  
+// }  
+
+
+// $num=12;  
+// if($num%2==0){  
+// echo "$num is even number \n";  
+// }else{  
+// echo "$num is odd number \n";  
+// }  
+
+// echo($num%2!=0?"Not Even \n":"Even \n");
+
+// $n=1;    
+// while($n<=10){    
+// echo "$n \n";    
+// $n++;    
+// }  
+
+
+?>  
--- Array/zero.php
+++ Array/zero.php
@@ -1,4 +1,26 @@
 <?php  
-$season=array("summer","winter","spring","autumn");  
-echo "Season are: $season[0], $season[1], $season[2] and $season[3]";  
+// $season=array("summer","winter","spring","autumn");  
+// echo "Season are: $season[0], $season[1], $season[2] and $season[3]";  
+
+
+// $fp=fopen("test.txt","w");
+// var_dump($fp);
+
+// $fp = fopen("test.txt", "w");
+// echo get_resource_type($fp) . "
+// ";
+
+// define("pi",3.1549);
+// echo pi;
+
+// define("WISHESH","Good morning sir Ashish");
+// function test(){
+//     echo WISHESH;
+// }
+// test()
+
+//****************************** */
+
+
 ?>  
+

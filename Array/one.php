e93e6228078ff1ed8415aa6d95d727b55cafd71c
Vikash Nayan <imvikashnayan@gmail.com>
2024-08-27 10:29:48 +0530

    read

--- Array/one.php
+++ Array/one.php
@@ -14,11 +14,44 @@
 
 // echo($num%2!=0?"Not Even \n":"Even \n");
 
-// $n=1;    
-// while($n<=10){    
-// echo "$n \n";    
-// $n++;    
-// }  
+$n=1;    
+while($n<=50){ 
+    if($n%2==0){  
+       echo "$n \n";
+        }   
+    
+$n++;    
+}  
+
+// $color = array("red","blue","white","green","black","green","orange") ;
+// foreach( $color as $c ) {  
+//     echo "color: ". $c ." ";
+
+// }
+
+// $details = array(
+//     "name"=> "vikash nayan \n",
+//     "email"=> "vikasjh@gmail.com \n",
+//     "age"=> "100 \n",
+// );
+// foreach ($details as $key => $value) {
+//     echo"". $key .": ". $value ."";
+// }
+
+// echo "welcome to lpu" , "<br>";
+// echo strlen("welcome to lpu"),"<br>";
+// echo strpos("welcome to lpu", "lpu"),"<br>";
+// echo str_word_count("welcome to lpu"),"<br>";
+
+
+$result = array(
+  array(
+    "name"=> "vikash ",
+    "cgpa"=> 7.8,
+    "status"=> "fail",
+  )
+)
+
 
 
 ?>  
---
+++ Array/read.php
@@ -0,0 +1,15 @@
+<?php
+$str1 = readline("Enter your name: ");
+$str2 = readline("Enter your State: ");
+$num1 = (int)readline("Enter your reg num: ");
+echo "\n";
+echo "Your full name: " . $str1 . "\n";
+echo "Your state: " . $str2 . "\n";
+echo "Your reg number: " . $num1 . "\n";
+// echo"\n";
+// echo"sum = ". ($num1+$num2) . "\n";
+// echo "diff = ". ($num1-$num2) . "\n";
+// echo "mult = ". ($num1*$num2) . "\n";
+// echo "quot ". ($num1/$num2) . "\n";
+// echo "expo = ". ($num1**$num2) . "\n";
+?>

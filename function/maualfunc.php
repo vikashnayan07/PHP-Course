0e13f9e00c8c993712e065a7fa0b9a961ea07f76
Vikash Nayan <imvikashnayan@gmail.com>
2024-08-30 12:41:28 +0530

    function...

--- function/maualfunc.php
+++ function/maualfunc.php
@@ -22,3 +22,42 @@ echo "Sum of two num is "  . $sum,"\n";
 
 
 ?>
+<?php
+$a = (int)readline("Enter the value: ");
+function fact($a){
+    $ans = 1;
+    while($a>0){
+        $ans = $a*$ans;
+        $a--;
+    }
+
+    echo "$ans";
+}
+fact($a);
+?>
+
+<?php
+$a = (int)readline("Enter the value: ");
+function fab($a){
+    if($a ==1 || $a ==0) return $a;
+    return fab($a -1) + fab($a -2);
+
+}
+echo fab($a);
+?>
+
+<?php
+$a = (int)readline("input 1:");
+$b = (int)readline("input 2:");
+
+function swap(& $a, & $b){
+    $rev = $a;
+    $a = $b;
+    $b = $rev;
+}
+
+swap($a,$b);
+echo "a:  . $a ,  b: " .$b;
+
+
+?>
